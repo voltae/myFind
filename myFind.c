559e7c32fbea92820b288910c9d857471cded979
voltae <ic17b096@technikum-wien.at>
2018-03-10 17:38:23 +0100

    changed the struct from point to local one. stat takes the adress from the local adress. TODO: the free does crash.

--- myFind.c
+++ myFind.c
@@ -92,8 +92,7 @@ int main(int argc, char **argv) {
 int do_dir(const char *dirName, const char **param) {
 
     char *filename;
-    /* define the path char array, start with a fixed length size
-     * was 100 , has to be much longer trying 500 */
+
     /* FIXME: Don't forget the free */
     char *basePath = malloc(sizeof(dirName));
     char *filePath = NULL;
@@ -127,25 +126,33 @@ int do_dir(const char *dirName, const char **param) {
         /* create the full filepath for found file, add 2 extra spaces for  '/' and '\0' */
         int lengthOfPath = (strlen(filename) + strlen(basePath)) + 2;
         /* FIXME: Don't forget the free the filepath */
-        filePath = malloc(lengthOfPath * sizeof(char));
+        if (!(filePath = malloc(lengthOfPath * sizeof(char))))
+        {
+            fprintf(stderr, "Memory error: %s\n", strerror(errno));
+        }
 
-        error = (snprintf(filePath, lengthOfPath + 1, "%s/%s", basePath, filename));
+        strcat(filePath, basePath);
+        strcat(filePath, "/");
+        strcat((filePath), filename);
+
+        // error = (snprintf(filePath, lengthOfPath + 1, "%s/%s", basePath, filename));
         if (error < 0) {
             fprintf(stderr, "An error occurred, %s\n", strerror(errno));
         }
 
         /* call the do_file function with the file path */
-        printf("Before calling do_file filePath: %s\n", filePath);
         do_file(filePath, param);
 
-        /*
-         printf("Filename: [%s/%s]\n", basePath, pdirent->d_name);
-         /* printf("User: [%s]", pdirent->)
+
+        printf("Filename: [%s/%s]\n", basePath, pdirent->d_name);
+        printf("File is Directory: %s\n", (pdirent->d_type == DT_DIR)? "yes" : "no");
+        /* printf("User: [%s]", pdirent->)
+
 
          if (pdirent->d_type == DT_DIR) {
          filename = pdirent->d_name;
 
-         / test the recursion , leave out the "." and ".." directory
+         /* test the recursion , leave out the "." and ".." directory
          if ((strcmp(filename, ".") != 0) && (strcmp(filename, "..") != 0)) {
 
          /* TODO: replace sprintf with snprintf (Buffer overflow)
@@ -176,29 +183,31 @@ int do_dir(const char *dirName, const char **param) {
 int do_file(const char *filename, const char **parms) {
     /* here comes the implementation of the new function */
     /* st is a struct of type stat for each file, in the stat struct is the information about the file */
-    struct stat *st = NULL;
+    struct stat st;
 
     /* iterator for while loop */
     int i = 0;
 
-    while (parms[i] != NULL) {
-        printf("Filepath stat: %s\n", filename);
-        if (stat(filename, st) == -1) {
-            fprintf(stderr, "Error in stat: %s\n", strerror(errno));
 
-            /* in case of failure return to the do_dir function and read in the next entry */
-            return -1;
-        }
+    if (stat(filename, &st) == -1) {
+        fprintf(stderr, "Error in stat: %s\n", strerror(errno));
 
-        printf("File path: %s\n", filename);
-
+        /* in case of failure return to the do_dir function and read in the next entry */
+        return -1;
     }
+    i++;
+    printf("File path: %s\n", filename);
 
-    printf("Filename: %s\n", filename);
 
-    if (S_ISDIR(st->st_mode)) {
+    if (S_ISDIR(st.st_mode)) {
         printf("this is a directory!\n");
+        /* call do_dir with this filempath to start a new recusrion to step in this directory */
+        do_dir(filename, parms);
+
+    } else {
+        printf("this is a regular file!\n");
     }
+    printf("Uid: %d\n", st.st_uid);
 
     return 0;
 }
