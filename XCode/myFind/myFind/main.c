98285ef8b61b39fe97217c88d787020c45550a70
voltae <ic17b096@technikum-wien.at>
2018-03-10 17:34:26 +0100

    Changed the st struct from pointer to local struct. The stat system call takes the adress of this struct. The function call stat works now as expected. The free calls of the basePath and the filePath crashes.

---
+++ XCode/myFind/myFind/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  myFind
+//
+//  Created by Valentin Platzgummer on 08.03.18.
+//  Copyright © 2018 Valentin Platzgummer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
