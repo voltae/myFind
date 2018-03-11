ab8a257d2e1270759c4f111c7aa43118574ab4e9
voltae <ic17b096@technikum-wien.at>
2018-03-11 10:11:44 +0100

    Fixed the second free bug. The termination '\0' place in basePath was missing. The second bug was created by the strncat function. By replacing it with snprintf all works fine.

--- myFind.c
+++ myFind.c
@@ -19,6 +19,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+
+/* ------------------------------------------------------- defines -----*/
+#define PATHDIVIDER "/"
 /* ------------------------------------------------------- functions -- */
 
 /* ------------------------------------------------------- const char --*/
@@ -71,7 +74,7 @@ int main(int argc, const char **argv) {
         }
     }
 
-   // const char *basePath = argv[1];
+    // const char *basePath = argv[1];
     /* call the openDirectory function for stepping though the directory */
 
     error = do_dir(argv[1], argv + 2);
@@ -83,20 +86,18 @@ int main(int argc, const char **argv) {
 }
 
 /**
- * @param dirName
+ * @param dirName char Pointer holding path of the found directory.
+ * @param param Pointer of charpointer holding the actions.
  * @return int 0 in case in regular case. -1 in error case.
  *
  */
 int do_dir(const char *dirName, const char **param) {
 
-    char *filename;
-
     /* Error was produced by this line: char *basePath = malloc(sizeof(dirName)); */
     /* basePath was f course way to short for holding the entire string, now corrected */
     char *basePath = malloc((strlen(dirName) + 1) * sizeof(char));
-    //char *filePath = NULL;
-    char filePath[500];
-    
+    char *filePath = NULL;
+
     /* declare a pointer to a DIR (directory datatype) */
     DIR *directory;
     /* The opendir function opens and returns a directory stream for reading the directory whose file name is dirname. The stream has type DIR */
@@ -105,8 +106,9 @@ int do_dir(const char *dirName, const char **param) {
     /* definition of opendir, https://github.com/lattera/glibc/blob/master/sysdeps/unix/opendir.c */
     directory = opendir(dirName);
 
-    /* copy the root directory name to the path string, this is the root of the directory path  */
-    strcpy(basePath, dirName);
+    /* copy the root directory name to the path string, this is the root of the directory path,
+     * copies the most length of dirName char, buffer overflow, don't forget the place for the last terminating '\0' */
+    strncpy(basePath, dirName, strlen(dirName) + 1);
 
     if (directory == NULL) {
         fprintf(stderr, "An error occurred during open dir: %s \n%s\n", dirName, strerror(errno));
@@ -124,34 +126,37 @@ int do_dir(const char *dirName, const char **param) {
             continue;
 
         /* create the full filepath for found file, add 2 extra spaces for  '/' and '\0' */
-        int lengthOfPath = (int)(strlen(filename) + (int)strlen(basePath) + 3);
+        int lengthOfPath = (int)(strlen(filename) + (int)strlen(basePath) + 2);
+
         /* FIXME: Don't forget the free the filepath, all work with a static char array. the error is presumable a buffer overrun in this pointer.
-         means the malloc assigment is to short. tarck that bug
+         means the malloc assigment is to short. tarck that bug */
         if (!(filePath = malloc(lengthOfPath * sizeof(char))))
         {
             fprintf(stderr, "Memory error: %s\n", strerror(errno));
         }
-         */
-        strcat(filePath, basePath);
-        strcat(filePath, "/");
-        strcat((filePath), filename);
 
-        // error = (snprintf(filePath, lengthOfPath + 1, "%s/%s", basePath, filename));
+        /*  int snprintf(char *str, size_t size, const char *format, ...);  creates the path in the buffer filePath at most lengthOfPath
+         * characters long, buffer overflow */
+        error = (snprintf(filePath, lengthOfPath + 2, "%s/%s", basePath, filename));
         if (error < 0) {
             fprintf(stderr, "An error occurred, %s\n", strerror(errno));
         }
-
+        /*
+        strncat(filePath, basePath, strlen(basePath));
+        strncat(filePath, PATHDIVIDER, strlen(PATHDIVIDER));
+        strncat(filePath, filename, strlen(filename));
+         */
         /* call the do_file function with the file path */
         do_file(filePath, param);
-
 
         printf("Filename: [%s/%s]\n", basePath, pdirent->d_name);
         printf("File is Directory: %s\n", (pdirent->d_type == DT_DIR)? "yes" : "no");
 
         /* free the filePath created in here */
-        //free(filePath);
-       // filePath = NULL;
-        filePath[0] = 0;
+        free(filePath);
+        filePath = NULL;
+        filename = NULL;
+
     }
 
     /* free the outer basePath */
