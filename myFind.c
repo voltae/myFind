900fae7a58b462968742b8decd422e5f1f4a1c7d
voltae <ic17b096@technikum-wien.at>
2018-04-02 15:16:40 +0200

    Bug 1 - Segfault -user. Fixed. > Bug 2 - Endless loop on user. Fixed > Bug 3 - -name followed only by 1 argument. Fixed idem -user > Bug 4 - print ls and nouser should not have any arguments. Fixed > Bug 5 - type should use only a certain pattern of chars. Fixed > Bug 6 - All error messages shoud be preceeded by progam name. Fixed. > TODO: Make all recursive calls of do_file with relative path. > type filtering." error: pathspec 'Bug 1 - Segfault -user. Fixed. Bug 2 - Endless loop on user. Fixed Bug 3 - -name followed only by 1 argument. Fixed idem -user Bug 4 - print ls and nouser should not have any arguments. Fixed Bug 5 - type should use only a certain pattern of chars. Fixed Bug 6 - All error messages shoud be preceeded by progam name. Fixed. TODO: Make all recursive calls of do_file with relative path. type filtering.' did not match any file(s) known to git.

--- myFind.c
+++ myFind.c
@@ -55,6 +55,8 @@ typedef enum {
     loopCont, loopExit
 } do_loop;
 
+typedef enum nameFound {FOUND, NOTFOUND} nameFound;
+
 /* ------------------------------------------------------- const char --*/
 const char *print = "-print";
 const char *user = "-user";
@@ -62,6 +64,7 @@ const char *uid = "-uid";
 const char *type = "-type";
 const char *name = "-name";
 const char *ls = "-ls";
+const char *nouser = "-nouser";
 
 
 /** @brief implementation of a simplified find programm
@@ -77,36 +80,91 @@ int main(int argc, const char **argv) {
     
     /* if no arguments are provided, print out a use message, and exit with failure */
     if (argc < 2) {
-        printf("usage: <file or directory> [-user <name>] [-name <pattern>] [-type [bcdpfls] [-print] [-ls]\n");
+        printf("myFind: Usage: <file or directory> [-user <name>] [-name <pattern>] [-type [bcdpfls] [-print] [-ls]\n");
         exit(EXIT_FAILURE);
     }
     
-    /* validate argv actions (begins always with a -) against menu action */
+    /* validate the actions of myFind step by step in a loop */
     for (int i = 0; i < argc; ++i) {
+        /* wheed out every -commmand that is not allowd */
         if (argv[i][0] == '-') {
             if (strcmp(argv[i], print) && strcmp(argv[i], user) && strcmp(argv[i], uid) && strcmp(argv[i], type) &&
-                strcmp(argv[i], name)
-                && strcmp(argv[i], ls)) {
-                printf("usage: <file or directory> [-user <name>] [-name <pattern>] [-type [bcdpfls] [-print] [-ls]\n");
+                strcmp(argv[i], name) && strcmp(argv[i], nouser) && (strcmp(argv[i], ls))) {
+                printf("myFind: Usage: <file or directory> [-user <name>] [-name <pattern>] [-type [bcdpfls] [-print] [-ls] [-nouser]\n");
                 exit(EXIT_FAILURE);
             }
         }
-        
+        /* if a valid action is found -name, -type but the next argument is missing, print out an error message. */
         if (!(strcmp(argv[i], user) && strcmp(argv[i], uid) && strcmp(argv[i], type) && strcmp(argv[i], name))) {
             if (argc == i + 1) {
-                printf("Missing argument for %s\n", argv[i]);
+                printf("myFind: Missing argument for %s\n", argv[i]);
                 exit(EXIT_FAILURE);
             }
         }
+        /* Test if the argument followed by the parameter of type -name is only a single one */
+        if (!(strcmp(argv[i], name)) && argv[i+2])
+        {
+            if (argv[i+2][0] != '-')
+            {
+                fprintf(stderr, "myFind: paths must precede expression: %s\n", argv[i+2]);
+                exit(EXIT_FAILURE);
+            }
+        }
+        /* Test if the argument followed the parameter -user is only a single one */
+        else if (!(strcmp(argv[i], user)) && argv[i+2])
+        {
+            if (argv[i+2][0] != '-')
+            {
+                fprintf(stderr, "myFind: user must precede expression: %s\n", argv[i+2]);
+                exit(EXIT_FAILURE);
+            }
+        }
+        /* Test if the argument followed the parameter -user is only a single one */
+        else if (!(strcmp(argv[i], type)) && argv[i+2])
+        {
+            if (argv[i+2][0] != '-')
+            {
+                fprintf(stderr, "myFind: %s: unknown primary or operator\n", argv[i+2]);
+                exit(EXIT_FAILURE);
+            }
+        }
+        /* -print, -ls, and -nouser must be single parameter, not followed by an argument */
+        else if ((!(strcmp(argv[i], print)) && argv[i+1]) || (!(strcmp(argv[i], ls)) && argv[i+1]) || (!(strcmp(argv[i], nouser)) && argv[i+1]))
+        {
+            {
+                fprintf(stderr, "myFind: %s: unknown primary or operator\n", argv[i+1]);
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        /* -type is followed ony by [bcdpfls] */
+         else if (!(strcmp(argv[i], type)))
+         {
+             nameFound allowedCharFound = NOTFOUND;
+             int j = 0;
+             char *allowedChar = NULL;
+             char allowedChars[7] = "bcdpfls";
+             do
+             {
+                 allowedChar = strchr(argv[i+1], allowedChars[j++]);
+                 if (!allowedChar) allowedCharFound = NOTFOUND;
+                 else allowedCharFound = FOUND;
+            } while (allowedCharFound == NOTFOUND && j < 7);
+             
+             if (allowedCharFound == NOTFOUND)
+             {
+                 printf("myFind: -type: %s: unknown type\n", argv[i+1]);
+                 exit(EXIT_FAILURE);
+             }
+        }
+        
+                  
     }
     
-    
     /* call the openDirectory function for stepping though the directory */
-    
     error = do_file(argv[1], &argv[2]);
-    /* log out itf an error occured */
+    /* log out if an error occured */
     if (error) {
-        
         return 0;
     }
 }
@@ -138,7 +196,7 @@ int do_dir(const char *dirName, const char **param) {
     
     
     if (directory == NULL) {
-        fprintf(stderr, "An error occurred during open dir: %s \n%s\n", dirName, strerror(errno));
+        fprintf(stderr, "myFind: An error occurred during open dir: %s \n%s\n", dirName, strerror(errno));
         return -1;
     }
     
@@ -166,11 +224,6 @@ int do_dir(const char *dirName, const char **param) {
         if (error < 0) {
             fprintf(stderr, "An error occurred, %s\n", strerror(errno));
         }
-        /*
-         strncat(filePath, basePath, strlen(basePath));
-         strncat(filePath, PATHDIVIDER, strlen(PATHDIVIDER));
-         strncat(filePath, filename, strlen(filename));
-         */
         
         /* call the do_file function with the file path */
         do_file(filePath, param);
@@ -225,8 +278,6 @@ int do_file(const char *filename, const char **parms) {
         
         /* if the action -print is given, step one element in the action further,
          * it might followed by -ls */
-       // else if (!strcmp(parms[i], print)) i++;
-        
         else if (!strcmp(parms[i], print))
         {
             printf("%s\n", filename);
@@ -245,7 +296,8 @@ int do_file(const char *filename, const char **parms) {
         else if (!strcmp(parms[i], name))
         {
             /* if is followed by a parameter, take this as valid name */
-            if (parms[i + 1]) {
+            if (parms[i + 1])
+            {
                 /* if found a name - match in path */
                 if (isNameInFilename(filename, parms[i+1])) i += 2;
                 /* if not, leave the loop, with no output */
@@ -254,22 +306,35 @@ int do_file(const char *filename, const char **parms) {
             /* if no parameter was found, print the error, no argument for name found */
             else
             {
-                printf("Missing Argument for -name\n");
+                printf("myFind: Missing Argument for \"-name\"\n");
                 exit(EXIT_FAILURE);
             }
         }
         
         /* if the action -user is found, step through following cases: */
-        else if (strcmp(parms[i], user) == 0) {
+        else if (!strcmp(parms[i], user))
+        {
+            char *userUIDString;
             /* calculate the length of the given uid in characters + 2 for the terminating 0, note log is natural, log10 is log based 10 */
-            int userLength = (floor(log10(st.st_uid)) + 2);
-            /* allocate the char string for the uid */
-            char *userUIDString = malloc(userLength * sizeof(char));
-            /* write the number as string, to be comparable with parameter char */
-            snprintf(userUIDString, userLength, "%u", st.st_uid);
-            
-            if (parms[i + 1]) {
-                
+            /* this works only if the entry in st.uid is > 0 */
+            if (st.st_uid > 0)
+            {
+                int userLength = (floor(log10(st.st_uid)) + 2);
+                /* allocate the char string for the uid */
+                userUIDString = malloc(userLength * sizeof(char));
+                /* write the number as string, to be comparable with parameter char */
+                snprintf(userUIDString, userLength, "%u", st.st_uid);
+            }
+            /* initialize userUIDString with '0' */
+            else
+            {
+                userUIDString = malloc(2 * sizeof(char));
+                userUIDString[0] = '0';
+                userUIDString[1] = 0;
+            }
+            if (parms[i + 1])
+            {
+                /* Compare the user string with the entry from system known user. Caution, the entry can differ from a known user */
                 if (((strcmp(userUIDString, parms[i + 1])) == 0) || readUID(st.st_uid, parms[i + 1]))
                 {
                     i += 2;
@@ -282,62 +347,113 @@ int do_file(const char *filename, const char **parms) {
                     free(userUIDString);
                 }
             }
-            else {
-                printf("Missing Argument for -name\n");
+            else
+            {
+                printf("myFind: Missing Argument for \"-name\"\n");
                 exit(EXIT_FAILURE);
             }
         }
         
-        else if (strcmp(parms[i], type)) {
-            /*Check on type */
+        else if (!strcmp(parms[i], type))
+        {
+            /*Check on type
+             *  b   ... block
+             *  c   ... character unbuffered special
+             *  d   ... directory
+             *  p   ... named pipe  (FIFO)
+             *  f   ... regular file
+             *  l   ... symolic link
+             *  s   ... socket
+             */
+            
+            /* increment the counter */
+            i++;
         }
         
-        else if (strcmp(parms[i], ls)) {
+        else if (!strcmp(parms[i], ls))
+        {
             out = lsOut;
             i++;
-            
         }
+        
+        /* if no valid argument is found, increment the counter */
+        else
+        {
+            i++;
+        }
+        
     } while (parms[i] && (looping == loopCont));
     
+    /* Output the filepath in -print format, only the path */
     if (out == regOut) {
-        printf("File path: %s\n", filename);
+        printf("%s\n", filename);
     }
+    /* Output the path in extended format with all parameters -ls */
     else if (out == lsOut) {
-        printf("File formatted ls out: %s\n", filename);
+        printf("%s\n", filename);
     }
-    
     
     if (S_ISDIR(st.st_mode)) {
-        //  printf("this is a directory!\n");
         /* call do_dir with this filempath to start a new recusrion to step in this directory */
         do_dir(filename, parms);
-        
     }
-    else
-    {
-        //  printf("this is a regular file!\n");
-    }
-    // printf("Uid: %d\n", st.st_uid);
     
     return 0;
     
 }
-
+/* read the user entry from system library. Check if the entry exists anyway */
 int readUID(uid_t uid, const char *name) {
     struct passwd *pwd = getpwuid(uid);
     
-    return !(strcmp(name, pwd->pw_name));
+    /* User is not in the Struct list. Leave the program, no further search is needed*/
+    if (pwd == NULL)
+    {
+        fprintf(stderr, "myFind: \"%s\" is not the name of a known user.\n", name);
+        exit(EXIT_FAILURE);
+    }
+    /* User is in the struct list */
+    /* test if the name and the user matches */
+    else if (!(strcmp(name, pwd->pw_name)))
+        return 1;
+    else
+        return 0;
 }
 
 char *getNameFromUID(uid_t uid)
 {
     struct passwd *pwd = getpwuid(uid);
-    return pwd->pw_name;
+    /* Check if the number doe correspont to a valid entry  in the system table known users and groups */
+    if (pwd == NULL)
+    {
+        /* calculate the length of the given uid in characters + 2 for the terminating 0, note log is natural, log10 is log based 10 */
+        int userLength = (floor(log10(uid)) + 2);
+        char *uidAsNumber = malloc(userLength * sizeof(char));
+        snprintf(uidAsNumber, 9, "%u", uid);
+        return uidAsNumber;
+    }
+    /* group is found, check if the entry matches */
+    else
+    {
+        /* set the enum user known */
+        return pwd->pw_name;
+    }
 }
+/** function returns a groupname with a systemcall
+ * if the group does not exist, return groupID as string */
 char *getNameFromGID(gid_t gid)
 {
     struct group *pwd = getgrgid(gid);
-    return pwd->gr_name;
+    /* Check if the number does correspont with a valid group number in the system */
+    if (pwd == NULL)
+    {
+        /* calculate the length of the given gid in characters + 2 for the terminating 0, note log is natural, log10 is log based 10 */
+        int groupLength = (floor(log10(gid)) + 2);
+        char *gidAsNumber = malloc(groupLength * sizeof(char));
+        snprintf(gidAsNumber, groupLength, "%u", gid);
+        return gidAsNumber;
+    }
+    /* group exists, return the name */
+    else return pwd->gr_name;
 }
 char *isNameInFilename (const char *filePath, const char *name)
 {
@@ -346,7 +462,7 @@ char *isNameInFilename (const char *filePath, const char *name)
     
     /* if the path does not contain any '/' the filepath should be the filename,
      * in order to find the name in the root path as well */
-   if(strstr(filePath, "/"))
+    if(strstr(filePath, "/"))
         fileName=strrchr(filePath, '/')+1;
     else fileName=filePath;
     
@@ -361,12 +477,13 @@ void extendedFileOutputFromStat (const struct stat *fileStat, const char *filePa
     int sizePointer = sizeof(fileStat);
     // number of hardlinks in inode
     int linkAmount = fileStat->st_nlink;
-    // user Id  owner
+    /* user Id  owner. Caution, the entry could be an number with is not a system known user */
     int userID = fileStat->st_uid;
-    char *userName = getNameFromUID(userID); // TODO: Get the username from getpwnam
-    // group ID owner
+    char *userName = getNameFromUID(userID);
+    
     int groupID = fileStat->st_gid;
-    char *groupName = getNameFromGID(groupID); // TODO: Get the groupName from getpwnam
+    /* group ID owner. Caution, the entry could be an number with is not a system known user */
+    char *groupName = getNameFromGID(groupID);
     // file length
     long int fileLength = fileStat->st_size;
     // time of last modification
@@ -384,6 +501,7 @@ void extendedFileOutputFromStat (const struct stat *fileStat, const char *filePa
     printf("%lu\t %d \t %s\t %3d\t %s\t %s\t %6lu\t %s %s\n", inodeNr, sizePointer, permissionsChar, linkAmount, userName, groupName, fileLength, timeFormatted, filePath);
     free(timeFormatted);
     free(permissionsChar);
+    
 }
 char *combineFilePermissions (mode_t fileMode)
 {
@@ -414,7 +532,7 @@ char *combineFilePermissions (mode_t fileMode)
     if ((fileMode & S_IXOTH) && (fileMode & S_ISVTX)) permissions[i++] = 't';
     else if (fileMode & S_ISVTX) permissions[i++] = 'T';
     else if (fileMode & S_IXOTH) permissions[i++] = 'x';
-   
+    
     permissions[i] = 0;
     return permissions;
 }
